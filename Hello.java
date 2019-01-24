8e1db6c275a0e1ddd4fc16642f8959be43ea3be8
thjee <43361160+thjee@users.noreply.github.com>
2019-01-23 21:27:59 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,8 +4,11 @@
 import java.util.Scanner;
 public class Hello {//open class
 	public static void main(Scanner keyboard){//opening main
+		Scanner keyboard = new Scanner(System.in); 
 		System.out.println("Enter your name: ");
 		String name = keyboard.nextLine();
 		System.out.println("You have entered your name as: "+name);
+		String name1 = keyboard.nextLine();
+		System.out.println("You have entered your name as: "+name1); //second contributers name
 	}//closing main
 }//close class
